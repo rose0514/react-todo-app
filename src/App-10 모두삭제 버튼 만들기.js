e243358da826fec67a18b2a2f4e25ba20fbd3883
rose0514 <rose0514_@naver.com>
2023-01-16 12:31:15 +0900

    최종 파일

---
+++ src/App-10 모두삭제 버튼 만들기.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import Lists from './components/Lists';
+import Form from './components/Form';
+export default function App() {
+
+
+
+
+const [ todoList, setTodoList ] = useState([]);
+
+
+
+
+const [value, setValue] = useState("");
+
+const btnSubmit = (e) => {
+  e.preventDefault();
+
+
+  let newTodo = {
+    id: Date.now(),
+    title: value,
+    completed: false
+  }
+  setTodoList(prev=> [...prev, newTodo])
+  setValue("");
+}
+
+const deleteAll = () => {
+  setTodoList([]);
+}
+
+
+  return (
+    <div className='container'>
+      <div className='todoBlock'>
+        <div className='title'>
+          <h1>To Do List</h1>
+          <button className='deleteBtn' onClick={deleteAll}>Delte All</button>
+        </div>
+        <Lists todoList={todoList} setTodoList ={setTodoList}/>
+        {/* 닫힘태그'/'중요! */}
+        <Form value={value} setValue={setValue} btnSubmit={btnSubmit}/>
+
+
+      </div>
+    </div>
+  )
+}
